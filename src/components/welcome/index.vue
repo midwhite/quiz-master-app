c246dfc5e6e7910481fcf18e24f12281ffa64639
Hiroki Takano <midwhite.development@gmail.com>
2018-07-08 19:46:39 +0900

    replace default components

---
+++ src/components/welcome/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="WelcomeComponent">
+    <h2>Welcome to QuizMaster!</h2>
+  </div>
+</template>
---
+++ src/root.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="app">
+    <h2>Root Component</h2>
+    <router-view />
+  </div>
+</template>
