4cfcc9a06a6c4ab1846e0cf173c0bb84d3856b26
Hiroki Takano <midwhite.development@gmail.com>
2018-07-15 21:58:25 +0900

    refactoring action-types

--- src/store/action-types.js
+++ src/store/action-types.js
@@ -2,54 +2,49 @@ import api from './api';
 
 /* eslint-disable arrow-body-style */
 export default {
-  getQuizzes: ({ commit }, params = {}) => {
+  getQuizzes({ commit }, params = {}) {
     return api.get('/v1/quizzes', params).then((data) => {
       commit('setMyQuizzes', data);
     });
   },
-  createQuiz: ({ commit }, { quiz }) => {
+  createQuiz({ commit }, { quiz }) {
     return api.post('/v1/quizzes', { quiz }).then((data) => {
       commit('addMyQuizzes', data);
       return data;
     });
   },
-  updateQuiz: ({ commit }, { quiz }) => {
+  updateQuiz({ commit }, { quiz }) {
     return api.put(`/v1/quizzes/${quiz.id}`, { quiz }).then((data) => {
       commit('replaceMyQuizz', data);
     });
   },
-  deleteQuiz: ({ commit }, { quiz }) => {
+  deleteQuiz({ commit }, { quiz }) {
     return api.delete(`/v1/quizzes/${quiz.id}`).then(() => {
       commit('removeMyQuiz', { quiz });
     });
   },
-  answerQuiz: ({ commit }, { quiz, answer }) => {
+  answerQuiz({ commit }, { quiz, answer }) {
     return api.post(`/v1/quizzes/${quiz.id}/check`, { answer }).then((data) => {
       commit('setResultToAnswer', data);
       return data;
     });
   },
-  signUp: ({ commit }, { form }) => new Promise((resolve, reject) => {
-    api.submit('/v1/users/sign_up', form).then((data) => {
+  signUp({ commit }, { form }) {
+    return api.submit('/v1/users/sign_up', form).then((data) => {
       commit('signIn', data);
-      resolve(data);
-    }).catch((error) => {
-      reject(error);
     });
-  }),
-  signIn: ({ commit }, { form }) => new Promise((resolve, reject) => {
-    api.submit('/v1/users/login', form).then((data) => {
+  },
+  signIn({ commit }, { form }) {
+    return api.submit('/v1/users/login', form).then((data) => {
       commit('signIn', data);
-      resolve(data);
-    }).catch((error) => {
-      reject(error);
     });
-  }),
-  getMe: ({ commit }) =>
-    api.get('/v1/users/me').then((data) => {
+  },
+  getMe({ commit }) {
+    return api.get('/v1/users/me').then((data) => {
       commit('signIn', data);
-    }),
-  signOut: ({ commit }) => {
+    });
+  },
+  signOut({ commit }) {
     return new Promise((resolve) => {
       commit('signIn', {});
       resolve();
